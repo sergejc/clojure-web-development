b4b9751b1ff325d65592e310dfedd190248ab783
Sergej Charskij <sergej.charskij@gmail.com>
2017-05-30 21:58:10 +0100

    created a template using selmer

---
+++ html-templating/src/html_templating/core.clj
@@ -0,0 +1,9 @@
+(ns html-templating.core
+  (:require [selmer.parser :as selmer]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(selmer/render-file "hello.html" {:name "World"})
